d67697626d307997ac5b9ff8523ff96754851707
Lénárt Bence <bence960206@gmail.com>
2019-02-22 11:10:16 +0100

    DB Project fixed

--- PetBellies/PetbelliesDBProjekt/dbo/Tables/Felhasznalok.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Felhasznalok] (
-    [ID]    INT           IDENTITY (1, 1) NOT NULL,
-    [Name]  NVARCHAR (50) NOT NULL,
-    [Email] NVARCHAR (50) NOT NULL,
-    [Age]   INT           NOT NULL,
-    [Reported] INT NOT NULL DEFAULT 0, 
-    PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
